c4a7188e0ebf79fdde561d4c5c7b39732986be56
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-10 09:37:54 +0300

    dummy usestate hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [customTitle, setCustomTitle] = useState(props.title);
+
+  const handleClick = () => {
+    setCustomTitle("CustomTitle");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{customTitle}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
+      <button onClick={handleClick}>change title</button>
     </Card>
   );
 };
